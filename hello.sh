468d9d0959c171b0c4b5c345454d4fa3c7e51025
shreyaheda8618 <shreyaheda451@gmail.com>
2026-05-06 21:17:19 +0530

    Fix echo command formatting in hello.sh

--- hello.sh
+++ hello.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
- echo "Learning shell scripting"
- echo "Linux basics"
+echo "Learning shell scripting"
+echo "Linux basics"
 echo "WSL setup"
 echo "AWS EC2 automation"
 echo "Shell scripting"
